5aeb2fbd1933a432e01aa6ba918b082214b8a9e1
unknown <pushkarchandra2005@gmail.com>
2026-07-25 18:46:40 +0530

    added config file

---
+++ week-1/Project1/Backend/app/core/config.py
@@ -0,0 +1,32 @@
+from functools import lru_cache
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # ==========================
+    # Application
+    # ==========================
+    APP_NAME: str = "AI Customer Support Platform"
+    APP_VERSION: str = "1.0.0"
+    DEBUG: bool = True
+
+    # ==========================
+    # API
+    # ==========================
+    API_V1_PREFIX: str = "/api/v1"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True,
+        extra="ignore",
+    )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
